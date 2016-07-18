afbd9a2b91cbccff9f6d71d3898b5cad7e6ea2bc
Zeejune <yzhijun3@gmail.com>
2016-07-18 13:31:53 +0800

    Add server side rendering

--- server/server.js
+++ server/server.js
@@ -3,8 +3,12 @@ import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpack from 'webpack';
 import webpackConfig from '../webpack.config.js';
-const app = express();
+import React from 'react';
+import { match, RouterContext } from 'react-router';
+import routes from '../client/routes';
+import { renderToString } from 'react-dom/server';
 
+const app = express();
 const compiler = webpack(webpackConfig);
 
 app.use(webpackDevMiddleware(compiler, {
@@ -14,23 +18,48 @@ app.use(webpackDevMiddleware(compiler, {
 
 app.use(webpackHotMiddleware(compiler));
 
-const renderFullPage = () => {
+const renderFullPage = (html) => {
   return `
     <!DOCTYPE html>
     <html>
     <body>
-      <div id="app"></div>
+      <div id="app">${html}</div>
       <script src="/bundle.js"></script>
     </body>
     </html>
   `;
 }
 
+const renderError = err => {
+  const softTab = '&#32;&#32;&#32;&#32;';
+  const errTrace = process.env.NODE_ENV !== 'production' ?
+    `:<br><br><pre style="color:red">${softTab}${err.stack.replace(/\n/g, `<br>${softTab}`)}</pre>` : '';
+  return renderFullPage(`Server Error${errTrace}`, {});
+};
+
 app.use((req, res, next) => {
-  res
-    .set('Content-Type', 'text/html')
-    .status(200)
-    .end(renderFullPage());
+  match({ routes, location: req.url }, (err, redirectLocation, renderProps) => {
+    if (err) {
+      return res.status(500).end(renderError(err));
+    }
+
+    if (redirectLocation) {
+      return res.status(302, redirectLocation.pathname + redirectLocation.search);
+    }
+
+    if (!renderProps) {
+      return next();
+    }
+
+    const initialView = renderToString(
+      <RouterContext {...renderProps} />
+    );
+
+    res
+      .set('Content-Type', 'text/html')
+      .status(200)
+      .end(renderFullPage(initialView));
+  })
 });
 
 app.listen(3000, () => {
